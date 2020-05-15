f6c3bb5b81966bdaaa04d214246b6161e782f781
enjibenji123 <48635154+enjibenji123@users.noreply.github.com>
2020-05-15 11:13:38 +1000

    demo ready

--- tasks.py
+++ tasks.py
@@ -10,9 +10,29 @@ def startJob(word,amount,wait):
         print(word)
         time.sleep(wait)
         x += 1
+    print('I have finished.')
 
 
 
 @huey.task()
 def startJobBatch(file,word,amount,wait):
     os.system((file) + " " + (word) + " " + (amount) + " " + (wait))
+
+
+
+
+
+
+#Demo tasks
+
+#  startJob('Hello', 5, 5)
+
+#  startJobBatch('JobBatch.bat', 'Hi', '4', '10')
+
+#  startJob('100', 2, 2, priority=100)
+#  startJob('75', 2, 2, priority=75)
+#  startJob('6', 2, 2, priority=6)
+
+#  startJobBatch('JobBatch.bat', 'Batch100', '2', '2', priority=100)
+#  startJobBatch('JobBatch.bat', 'Batch80', '2', '2', priority=80)
+#  startJobBatch('JobBatch.bat', 'Batch5', '2', '2', priority=5)
